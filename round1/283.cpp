ceac93392649d82237f698c49578cc77630846a4
XDTD <1355582364@qq.com>
2020-11-19 09:43:10 +0800

    2020.11.19 daily question important

---
+++ round1/283.cpp
@@ -0,0 +1,14 @@
+//https://leetcode-cn.com/problems/move-zeroes/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size(), left = 0, right = 0;
+        while (right < n) {
+            if (nums[right]) {
+                swap(nums[left], nums[right]);
+                left++;
+            }
+            right++;
+        }
+    }
+};
