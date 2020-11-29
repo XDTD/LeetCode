5c5b1fdb96555a9992a1db5e4cd25625199ac45d
XDTD <1355582364@qq.com>
2020-11-29 22:57:46 +0800

    2020.11.28(493) && 2020.11.29(976)   daily question

---
+++ round1/493.cpp
@@ -0,0 +1,60 @@
+//https://leetcode-cn.com/problems/reverse-pairs/
+class BIT {
+private:
+    vector<int> tree;
+    int n;
+
+public:
+    BIT(int _n) : n(_n), tree(_n + 1) {}
+
+    static constexpr int lowbit(int x) {
+        return x & (-x);
+    }
+
+    void update(int x, int d) {
+        while (x <= n) {
+            tree[x] += d;
+            x += lowbit(x);
+        }
+    }
+
+    int query(int x) const {
+        int ans = 0;
+        while (x) {
+            ans += tree[x];
+            x -= lowbit(x);
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        set<long long> allNumbers;
+        for (int x : nums) {
+            allNumbers.insert(x);
+            allNumbers.insert((long long)x * 2);
+        }
+        // 利用哈希表进行离散化
+        unordered_map<long long, int> values;
+        int idx = 0;
+        for (long long x : allNumbers) {
+            values[x] = ++idx;
+        }
+
+        int ret = 0;
+        BIT bit(values.size());
+        for (int i = 0; i < nums.size(); i++) {
+            int left = values[(long long)nums[i] * 2], right = values.size();
+            ret += bit.query(right) - bit.query(left);
+            bit.update(values[nums[i]], 1);
+        }
+        return ret;
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/reverse-pairs/solution/fan-zhuan-dui-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
