4668a1f5ffc4c52f0d919e7e908af170daf4c2ee
XDTD <34300181+XDTD@users.noreply.github.com>
2021-07-13 19:45:55 +0800

    简单的dp题，缺少组合数学的思路

---
+++ top100Round1/62.go
@@ -0,0 +1,29 @@
+package main
+
+//https://leetcode-cn.com/problems/unique-paths/submissions/
+//简单的dp题，缺少组合数学的思路
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		dp[i][0] = 1
+	}
+	for i := 0; i < n; i++ {
+		dp[0][i] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+func main8() {
+	m, n := 4, 5
+	ans := uniquePaths(m, n)
+	println(ans)
+}
